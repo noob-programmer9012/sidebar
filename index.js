27290d1c2e250bf060bc0ca1a407dd9e53d5c7d8
Parth Patel <parth.sclub@gmail.com>
2022-11-22 00:46:16 +0530

    filter and media queries

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
 const toggleNav = document.querySelector('#mobile')
 const sidebar = document.querySelector('.sidebar')
 const main = document.querySelector('.main')
+const modal = document.querySelector('#modal')
+const confirm = document.querySelector('#confirm')
+const closeModal = document.querySelectorAll('#modal-close')
+
+closeModal.forEach(item => {
+  item.addEventListener('click', () => {
+    modal.classList.remove('show-modal')
+  })
+})
+
+function filterTable () {
+  let input, filter, table, tr, td, i, txtValue
+  input = document.getElementById('search')
+  filter = input.value.toUpperCase()
+  table = document.getElementById('myTable')
+  tr = table.getElementsByTagName('tr')
+
+  // Loop through all table rows, and hide those who don't match the search query
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName('td')[0]
+    if (td) {
+      txtValue = td.textContent || td.innerText
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = ''
+      } else {
+        tr[i].style.display = 'none'
+      }
+    }
+  }
+}
 
 function clicked () {
   sidebar.classList.toggle('show')
   if (sidebar.style.width === '270px') {
-    sidebar.style.width = '70px'
+    sidebar.style.width = '60px'
   } else {
     sidebar.style.width = '270px'
   }
@@ -19,7 +49,6 @@ function setAttributes (elem, attr) {
 }
 
 let obj = new Object()
-
 for (let i = 0; i < localStorage.length; i++) {
   obj[localStorage.key(i)] = localStorage.getItem(localStorage.key(i))
 }
@@ -29,37 +58,61 @@ const save = document.querySelector('#save')
 
 function duplicate (transporter, gst) {
   for (const elem in obj) {
-    if (`${elem}` === transporter && `${obj[elem]}` === gst) {
+    if (
+      `${elem.toUpperCase().trim()}` === transporter.toUpperCase() &&
+      `${obj[elem].toUpperCase().trim()}` === gst.toUpperCase()
+    ) {
       return true
     }
   }
   return false
 }
 
+save.addEventListener('click', saveData)
 function saveData (e) {
-  let form = document.querySelector('form')
   e.preventDefault()
-  if (trans.value !== '' && gst.value !== '') {
+  if (trans.value !== '' || gst.value !== '') {
     if (e.target.innerText === 'Submit') {
-      // localStorage.setItem(trans.value, gst.value)
-      localStorage.setItem(trans.value, gst.value)
-      window.location.reload()
+      if (!duplicate(trans.value, gst.value)) {
+        localStorage.setItem(
+          trans.value.toUpperCase().trim(),
+          gst.value.toUpperCase().trim()
+        )
+        window.location.reload()
+      } else {
+        alert('Duplicate Entry')
+      }
     } else if (e.target.innerText === 'Change') {
       if (!duplicate(trans.value, gst.value)) {
         localStorage.removeItem(e.target.value)
-        localStorage.setItem(trans.value, gst.value)
-        form.reset()
+        localStorage.setItem(
+          trans.value.toUpperCase().trim(),
+          gst.value.toUpperCase().trim()
+        )
+        e.target.innerText = 'Submit'
+        save.click()
+      } else {
+        alert('Duplicate Entry')
       }
-      window.location.reload()
     }
   } else {
     alert('empty')
   }
 }
 
-save.addEventListener('click', saveData)
-
 let tr, td0, td1, td2, td3, key, value, edit, remove
+
+function removeData (e) {
+  localStorage.removeItem(e.target.value)
+  window.location.reload()
+}
+confirm.addEventListener('click', removeData)
+
+modal.addEventListener('click', e => {
+  if (e.target.id === 'modal') {
+    modal.classList.remove('show-modal')
+  }
+})
 
 document.addEventListener('DOMContentLoaded', () => {
   // Load Data from local storage
@@ -111,9 +164,11 @@ document.addEventListener('DOMContentLoaded', () => {
   remove = document.querySelectorAll('#remove')
   remove.forEach(item => {
     item.addEventListener('click', e => {
-      const modal = document.querySelector('#modal')
-      let modalBody = document.querySelector('.modal-body')
+      const modalBody = document.querySelector('.modal-body')
+      modalBody.innerHTML = `Delete ${data[e.target.title]}?`
       modal.classList.add('show-modal')
+      const confirm = document.querySelector('#confirm')
+      confirm.value = data[e.target.title]
     })
   })
 
@@ -125,6 +180,8 @@ document.addEventListener('DOMContentLoaded', () => {
       trans.value = data[e.target.title]
       gst.value = obj[trans.value]
       save.value = data[e.target.title]
+      const cancel = document.querySelector('#cancel')
+      cancel.classList.add('show')
     })
   })
 })
